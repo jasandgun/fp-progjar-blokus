cf8e472e456cbd7de354c2d7eafe8f42e57fdc3c
Jason Gunawan <jasandgun@gmail.com>
2022-06-02 17:00:46 +0700

    experimental: buggy server UI

--- src/server.py
+++ src/server.py
@@ -1,13 +1,13 @@
 import socket  # for networking
 import sys
 from threading import Thread  # for threading
-import pygame  # to display infobox
+import pygame  # to display ui
 import constants
 
 HOST = socket.gethostbyname(socket.gethostname())  # this address is the ipv4
 PORT = 8080  # port to listen on for clients
 
-# set up the server 
+# set up the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
@@ -46,26 +46,59 @@ def remove(connection):
 
 
 # accept a connection from the client
-try:
+# try:
+#     while True:
+#         client_socket, client_address = s.accept()
+#
+#         for client in list_of_clients:
+#             client.close()
+#         list_of_clients = [client_socket]
+#
+#         client_socket.send('p1'.encode())
+#         Thread(target=client_thread, args=(client_socket,)).start()
+#         print(f"\nConnected to {client_address}!")
+#
+#         client_socket, client_address = s.accept()
+#         list_of_clients.append(client_socket)
+#         client_socket.send('p2'.encode())
+#         Thread(target=client_thread, args=(client_socket,)).start()
+#         print(f"\nConnected to {client_address}!")
+#
+#         print("\nGAME START !!!")
+#
+# except KeyboardInterrupt:
+#     s.close()
+#     sys.exit()
+WIN = pygame.display.set_mode((constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT))
+
+
+def server_start():
+    global list_of_clients
     while True:
-        client_socket, client_address = s.accept()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                s.close()
+                sys.exit()
+            else:
+                client_socket, client_address = s.accept()
 
-        for client in list_of_clients:
-            client.close()
-        list_of_clients = [client_socket]
+                for client in list_of_clients:
+                    client.close()
+                list_of_clients = [client_socket]
 
-        client_socket.send('p1'.encode())
-        Thread(target=client_thread, args=(client_socket,)).start()
-        print(f"\nConnected to {client_address}!")
+                client_socket.send('p1'.encode())
+                Thread(target=client_thread, args=(client_socket,)).start()
+                print(f"\nConnected to {client_address}!")
 
-        client_socket, client_address = s.accept()
-        list_of_clients.append(client_socket)
-        client_socket.send('p2'.encode())
-        Thread(target=client_thread, args=(client_socket,)).start()
-        print(f"\nConnected to {client_address}!")
+                client_socket, client_address = s.accept()
+                list_of_clients.append(client_socket)
+                client_socket.send('p2'.encode())
+                Thread(target=client_thread, args=(client_socket,)).start()
+                print(f"\nConnected to {client_address}!")
 
-        print("\nGAME START !!!")
+                print("\nGAME START !!!")
 
-except KeyboardInterrupt:
-    s.close()
-    sys.exit()
+
+if __name__ == "__main__":
+    server_start()
